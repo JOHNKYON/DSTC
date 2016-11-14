60f43a6388f85d60fbccb92869308b742c531091
JOHNKYON <zhaofengbo1321@163.com>
2016-11-14 18:22:13 +0800

    Almost finish init

--- DSTC2/basic.py
+++ DSTC2/basic.py
@@ -4,8 +4,9 @@
 # from keras.models import Sequential
 # from keras.layers import Dense, Activation
 from DSTC2.traindev.scripts import myLogger
-from traindev.scripts import corpus
 from traindev.scripts import file_reader
+from traindev.scripts import initializer
+from traindev.scripts.initializer import InputRaw
 
 __author__ = "JOHNKYON"
 
@@ -18,7 +19,12 @@ if __name__ == "__main__":
     # 选择模式
     dataset = file_reader.get_dataset("dstc2_debug")
     logger.info("token check test begin")
-    for call in dataset:
-        # if call.log["session-id"] == "voip-f32f2cfdae-130328_192703":
-        for turn, label in call:
-            corpus.input_tokenize(turn["output"]["transcript"])
+    raw = initializer.raw_initializer(dataset)
+    # Build token and dictionary
+    token = initializer.token_initializer(raw["input"])
+    dictionary = initializer.dictionary_initializer(token)
+    # Build input vector
+    input_raw = InputRaw(token, dictionary)
+    for call in input_raw:
+        print call
+
