b843784dfa66870bde29145153c182040bc44826
JOHNKYON <zhaofengbo1321@163.com>
2016-11-16 19:03:09 +0800

    Finish init

--- DSTC2/basic.py
+++ DSTC2/basic.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-  
 
 from sklearn.cross_validation import train_test_split
+
 from DSTC2.traindev.scripts import myLogger
+from DSTC2.traindev.scripts.model import bp
 from traindev.scripts import file_reader
 from traindev.scripts import initializer
 from traindev.scripts.initializer import Set
-from traindev.scripts import bp
 
 __author__ = "JOHNKYON"
 
@@ -25,10 +26,9 @@ if __name__ == "__main__":
     # Build input vector
     one_set = Set(token, dictionary, raw["output"])
     # get model
-    model = bp.bp_builder(one_set.dimension * one_set.sentence_dim, len(one_set.act_dict)*one_set.sentence_dim)
+    model = bp.bp_builder(one_set.dimension * one_set.sentence_dim, len(one_set.act_dict) * one_set.sentence_dim)
     # train
     X_train, X_test, y_train, y_test = train_test_split(one_set.input_mtr, one_set.output_mtr, test_size=0.2)
     model.fit(X_train, y_train, batch_size=2, nb_epoch=5)
     # test
-    model.evaluate(X_test, y_test, batch_size=2)
-    print model.predict(X_test)
+    print model.evaluate(X_test, y_test, batch_size=2)
