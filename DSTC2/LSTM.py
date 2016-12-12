28bd2fbc261b7dd537a0661e5541bad74c153393
JOHNKYON <zhaofengbo1321@163.com>
2016-12-12 10:01:20 +0800

    LSTM finished

--- DSTC2/LSTM.py
+++ DSTC2/LSTM.py
@@ -8,17 +8,30 @@ from traindev.scripts import file_reader
 from traindev.scripts import initializer
 from traindev.scripts.initializer import Set
 from traindev.scripts.judge import recall_precision_F
+import codecs
+import matplotlib.pyplot as plt
 
 __author__ = "JOHNKYON"
 
 global logger
 
 if __name__ == "__main__":
+    # Choose mode
+    # 1:act 2:slot
+    mode = 2
+
     global logger
     logger = myLogger.myLogger("basic")
     logger.info("Starting basic")
+    record_file = codecs.open("threshold_record.txt", 'wb+', encoding='utf8')
+    threshold = 0.05
+    plt.figure(1)
+    # ready to plot
+    thres = []
+    f_m = []
     # 选择模式
-    for n in range(0, 4):
+    roll = range(0, 16)
+    for n in roll:
         dataset = file_reader.get_dataset("dstc2_debug")
         logger.info("token check test begin")
         raw = initializer.raw_initializer(dataset)
@@ -26,19 +39,39 @@ if __name__ == "__main__":
         token = initializer.token_initializer(raw["input"])
         dictionary = initializer.dictionary_initializer(token)
         # Build input vector
-        one_set = Set(token, dictionary, raw["output"])
+        one_set = Set(token, dictionary, raw["output"], mode)
         input_mtr, output_mtr = LSTM.basic_LSTM_init(one_set.input_mtr, one_set.output_mtr)
+
+
         # TODO:设计降维算法，需要将输出的维度降低。
         # get model
         # model = LSTM.get_basic_LSTM(one_set.sentence_length, len(one_set.act_dict))
         model = LSTM.get_LSTM(one_set.sentence_length, len(one_set.act_dict))
+
+
         # train
         X_train, X_test, y_train, y_test = train_test_split(input_mtr, output_mtr, test_size=0.2, random_state=True)
         model.fit(X_train, y_train, batch_size=16, nb_epoch=12)
+
+
         # test
         print model.evaluate(X_test, y_test, batch_size=2)
         y_pre = model.predict(X_test, batch_size=16)
-        # TODO: 新增阈值的自适应用能
-        print("[recall: %s,\tprecision: %s,\tf_measure: %s]", recall_precision_F(y_test, y_pre))
+        # print("[recall: {0},\tprecision: {1},\tf_measure: {2}]".format(recall_precision_F(y_test, y_pre)))
+
+
+        f_measure_old = 0
+        recall, precision, f_measure = recall_precision_F(y_test, y_pre, threshold)
+        f_m.append(f_measure)
+        thres.append(threshold)
+        record_file.write('threshold: {0}'.format(threshold))
+        threshold += (f_measure - f_measure_old) / 200
+
+
+        print("[recall: {0},\tprecision: {1},\tf_measure: {2}, threshold: {3}]".format(recall, precision, f_measure, threshold))
+        record_file.write("[recall: {0},\tprecision: {1},\tf_measure: {2}]\n\n".format(recall, precision, f_measure))
         print "--------------------------------------------------------------"
+    plt.plot(roll, f_m, 'b', roll, thres, 'r')
+    plt.savefig('figure.jpg')
+    record_file.close()
 
